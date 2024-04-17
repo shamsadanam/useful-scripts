dc85e3fd69f42c1b3cedf98fe203e1c568c1b6ec
Shamsad Anam <shamsad.anam@gmail.com>
2024-04-17 12:15:59 +0600

    feat: add pathname from arguments

--- generate-array.js
+++ generate-array.js
@@ -1,13 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-const directoryPath = path.join(
-  __dirname,
-  "public",
-  "frontend",
-  "temp",
-  "projects"
-);
+// Get the directory path from the command line arguments
+const directoryPath = process.argv[2];
+
 fs.readdir(directoryPath, function (err, files) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
