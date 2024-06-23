b3ebe6af790f996f6735972c5229bfa38c0b94dd
Shamsad Anam <57474743+shamsadanam@users.noreply.github.com>
2024-06-23 16:37:15 +0600

    feat: sort files by modified date

--- generate-array.js
+++ generate-array.js
@@ -1,21 +1,34 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 // Get the directory path from the command line arguments
 const directoryPath = process.argv[2];
 
-fs.readdir(directoryPath, function (err, files) {
+fs.readdir(directoryPath, (err, files) => {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
 
-  const array = files.map((file) => {
+  // Get metadata for each file
+  const filesWithStats = files.map(file => {
+    const filePath = path.join(directoryPath, file);
+    const stats = fs.statSync(filePath);
     return {
-      image: path.join(directoryPath, file),
-      name: file.replace(/-/g, " ").split(".")[0],
-      location: "",
+      file: file,
+      filePath: filePath,
+      modifiedTime: stats.mtime
     };
   });
+
+  // Sort files by modified time
+  filesWithStats.sort((a, b) => a.modifiedTime - b.modifiedTime);
+
+  // Create the array in the desired format
+  const array = filesWithStats.map((fileWithStats, index) => ({
+    image: fileWithStats.filePath,
+    name: fileWithStats.file.replace(/-/g, " ").split(".")[0],
+    order: index
+  }));
 
   console.log(array);
 });
